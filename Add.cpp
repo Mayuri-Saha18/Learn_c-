238ae2062d77d705e0faf0e1fcfb3161ff064309
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-08-24 17:06:55 +0530

    add

--- Add.cpp
+++ Add.cpp
@@ -8,3 +8,14 @@ int main() {
   cout << sum;
   return 0;
 }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int x, y, z;
+//   x = y = z = 50;
+//   cout << x + y + z;
+//   return 0;
+// }
