31615ad2da0d08391c6cd5b621215d2398f6a6e0
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-08-24 16:57:52 +0530

    a

---
+++ Newline.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World! \n";
+//   cout << "I am learning C++";
+//   return 0;
+// }
+
+
+
+// Tip: Two \n characters after each other will create a blank line:
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World! \n\n";
+//   cout << "I am learning C++";
+//   return 0;
+// }
+
+
+// Another way to insert a new line, is with the endl manipulator:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "I am learning C++";
+  return 0;
+}
