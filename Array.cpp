c260d2c5006d04aa0b00105581b037773f9e8f3e
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-08-26 17:20:39 +0530

    add

--- Array.cpp
+++ Array.cpp
@@ -8,3 +8,28 @@ int main() {
   }
   return 0;
 }
+
+
+// 2
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  cout << sizeof(myNumbers);
+  // 20
+  return 0;
+}
+// int type is usually 4 bytes, so from the example above, 4 x 5 (4 bytes x 5 elements) = 20 bytes.
+
+// 3
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  int getArrayLength = sizeof(myNumbers) / sizeof(int);
+  cout << getArrayLength;
+  return 0;
+}
+
